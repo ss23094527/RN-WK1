16c1d03477e1639206b2126792c910e55f8b6d6b
ss23094527 <ss23094527@gmail.com>
2024-02-20 16:29:43 +0800

    WK1 WORK

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
 
     
     <View style={styles.container}>
+      
+
       <Text style={styles.box}>NTUEDTD</Text>
+      <Image source={require('./assets/favicon.png')} style={styles.icon}/>
+     
       <Text  style={styles.font}>數位三甲 111019045 文奕婕</Text>
       <StatusBar style="auto" />
     </View>
@@ -20,11 +24,18 @@ const styles = StyleSheet.create({
     backgroundColor: 'yellow',
     
   },
+  icon:{
+    width: 80, 
+    height: 80,
+    marginTop:40,
+    marginLeft:170,
+  },
+   
   font:{
     color:'#000',
     fontSize:20,
     textAlign:'center',
-    margin:50,
+    margin:20,
     justifyContent:'center',
     fontWeight:'bold',
     fontFamily:'monospace',
@@ -32,14 +43,14 @@ const styles = StyleSheet.create({
   },
 
   box:{
-    color:'gray',
+    color:'white',
     shadowColor:'black',
     
     textAlign:'center',
     margin:0,
     padding:20,
     fontSize:18,
-    backgroundColor: '#00000',
+    backgroundColor: 'gray',
 
   },
 });
